aaef74a4f55e6e735756161aa4051547a95ef819
Gro0vy <lmc2819@naver.com>
2026-05-19 18:25:35 +0900

    사다리 타기 풀이

---
+++ src/simulation/LadderGame.java
@@ -0,0 +1,34 @@
+package simulation;
+
+import java.util.*;
+
+public class LadderGame {
+    public char[] solution(int n, int[][] ladder){
+        char[] answer = new char[n];
+
+        for(int i = 0; i < n; i++){
+            answer[i] = (char)('A' + i);
+        }
+
+        for(int[] level: ladder){
+            for(int start: level) {
+                start -= 1;
+                int end = start + 1;
+
+                char temp = answer[start];
+                answer[start] = answer[end];
+                answer[end] = temp;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        LadderGame T = new LadderGame();
+        System.out.println(Arrays.toString(T.solution(5, new int[][]{{1, 3}, {2, 4}, {1, 4}})));
+        System.out.println(Arrays.toString(T.solution(7, new int[][]{{1, 3, 5}, {1, 3, 6}, {2, 4}})));
+        System.out.println(Arrays.toString(T.solution(8, new int[][]{{1, 5}, {2, 4, 7}, {1, 5, 7}, {2, 5, 7}})));
+        System.out.println(Arrays.toString(T.solution(12, new int[][]{{1, 5, 8, 10}, {2, 4, 7}, {1, 5, 7, 9, 11}, {2, 5, 7, 10}, {3, 6, 8, 11}})));
+    }
+}
