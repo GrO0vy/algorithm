056742c45f537209d38ce869102e538aaf42968a
Gro0vy <lmc2819@naver.com>
2026-06-09 10:03:38 +0900

    다른 빈도수 만들기 풀이2

--- src/timeParsing/MakeDifferentFreq.java
+++ src/timeParsing/MakeDifferentFreq.java
@@ -8,30 +8,21 @@ public class MakeDifferentFreq {
     public int solution(String s){
         int answer = 0;
 
-        int[] freq = new int[CNT_CHAR];
-        for(int i = 0; i < s.length(); i++) freq[s.charAt(i) - 'a']++;
+        int[] cnt = new int[CNT_CHAR];
+        Set<Integer> used = new HashSet<>();
 
-        int maxFreq = 0;
-        int remain = 0;
-
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < CNT_CHAR; i++) {
-            map.put(freq[i], map.getOrDefault(freq[i], 0) + 1);
-            maxFreq = Math.max(maxFreq, freq[i]);
+        for(int i = 0; i < s.length(); i++){
+            cnt[s.charAt(i) - 'a']++;
         }
 
-        for(int i = maxFreq; i > 0; i--){
-            if(map.containsKey(i)){
-                answer += remain + map.get(i) - 1;
-                remain += map.get(i) - 1;
+        for(int c: cnt){
+            while(c > 0 && used.contains(c)) {
+                c--;
+                answer++;
             }
-            else{
-                remain = Math.max(0, remain - 1);
-                answer += remain;
-            }
-        }
 
-//        answer += Math.max(0, remain);
+            if(c > 0) used.add(c);
+        }
 
         return answer;
     }
