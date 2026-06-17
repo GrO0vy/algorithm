43ba48187d2f1cb8ac1d2781729844125226f85c
Gro0vy <lmc2819@naver.com>
2026-06-17 15:28:17 +0900

    최대 길이 연속수열 풀이2

--- src/dataStructure/LongestConsecutiveSequence.java
+++ src/dataStructure/LongestConsecutiveSequence.java
@@ -4,33 +4,31 @@ import java.util.*;
 
 public class LongestConsecutiveSequence {
     public int solution(int[] nums){
-        int answer = 1;
+        int answer = 0;
 
-        Arrays.sort(nums);
+        Set<Integer> set = new HashSet<>();
+        for(int num: nums) set.add(num);
 
-        int[] maxLen = new int[nums.length];
-        maxLen[0] = 1;
+        for(int num: set){
+            if(set.contains(num - 1)) continue;
 
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i - 1] + 1 == nums[i]){
-                maxLen[i] = maxLen[i - 1] + 1;
-                answer = Math.max(answer, maxLen[i]);
+            int len = 1;
+            while(set.contains(num + 1)){
+                len++;
+                num++;
             }
-            else if(nums[i - 1] == nums[i]) maxLen[i] = maxLen[i - 1];
-            else maxLen[i] = 1;
+
+            answer = Math.max(answer, len);
         }
 
         return answer;
     }
 
-    // 1. 정렬을 한다
-    // 2. 각 원소를 끝 원소로 했을 때의 최대 연속 수열 길이를 저장 할 배열 선언 ( maxLen )
-    // 3. maxLen 배열의 첫 원소를 1로 초기화
-    // 4. for 문으로 두 번 쨰 원소부터 모든 원소를 순회하며 아래의 과정 수행
-    // 4-1. 만약 이전 원소와의 차이가 1이라면 이전 원소의 연속 수열 길이 + 1 을 현재 원소의 연속 수열 길이로 한 후 이전까지의 최대 길이와 비교해서 최대 길이를 갱신한다.
-    // 4-2. 만약 이전 원소와 현재 원소가 같다면 이전 원소의 연속 수열 길이를 현재 원소의 연속 수열 길이로 한다.
-    // 4-3. 이외의 경우는 현재 원소의 연속 수열 길이를 1로 한다.
-    // 5. 결과로 최댓값을 리턴한다.
+    // 1. set 에 nums 의 원소를 저장한다 ( 중복제거 )
+    // 2. set 의 원소를 순회하면서 만약 원소 - 1 의 값이 set에 존재한다면 continue ( 시작점이 아닌 부분은 거른다 )
+    // 3. 시작점이라면 +1 을 해나가면서 연속된 원소의 개수를 센다.
+    // 4. 최대 길이와 연속된 원소의 개수를 비교한다.
+    // 5. 최대 길이를 반환한다.
 
     public static void main(String[] args){
         LongestConsecutiveSequence T = new LongestConsecutiveSequence();
